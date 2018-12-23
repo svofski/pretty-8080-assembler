7902278bd3a5a6061f11d2fa6fa142b21e723bb4
svo <svofski@gmail.com>
2018-12-23 19:33:57 +0300

    String.prototype.trimLeft() polyfill
    for microsoft edge compatibility

--- util.js
+++ util.js
@@ -85,3 +85,16 @@ Util.formatGutterFull = function(addr, bytes)
 
     return hexes;
 }
+
+// Microsoft Edge compatibility polyfill
+// https://github.com/KhaledElAnsari/String.prototype.trimStart
+String.prototype.trimStart = String.prototype.trimStart ? String.prototype.trimStart : function() {
+    if(String.prototype.trimLeft) {
+        return this.trimLeft();
+    } else if(String.prototype.trim) {
+        var trimmed = this.trim();
+        var indexOfWord = this.indexOf(trimmed);
+
+        return this.slice(indexOfWord, this.length);
+    }
+};
